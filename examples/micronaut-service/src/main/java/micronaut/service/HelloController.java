387439f06a060f43fa251a2d779eb1789799c62b
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2019-03-04 11:56:40 +0100

    Changes to publish the sample application images to Docker Hub.
    Also contains Kubernetes Service and Deployment definitions

--- examples/micronaut-service/src/main/java/micronaut/service/HelloController.java
+++ examples/micronaut-service/src/main/java/micronaut/service/HelloController.java
@@ -3,12 +3,18 @@ package micronaut.service;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.HttpStatus;
+import io.micronaut.runtime.server.EmbeddedServer;
+
+import javax.inject.Inject;
 
 @Controller("/hello")
 public class HelloController {
 
+    @Inject
+    EmbeddedServer embeddedServer;
+
     @Get("/{name}")
     public String index(final String name) {
-        return "Hello, " + name;
+        return "Hello, " + name + ". From " + embeddedServer.getHost() + ":" + embeddedServer.getPort() + "\n";
     }
 }
