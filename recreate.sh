385d737ea8ca236ccd1ab5e3ab6af373403b6be1
Sergio del Amo <sergio.delamo@softamo.com>
2019-03-05 17:15:04 +0100

    delete files for gihtub pages branch

--- recreate.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-kubectl delete deployments.apps example-service
-kubectl delete service example-service
-kubectl delete deployments.apps example-client
-kubectl delete service example-client
-./gradlew clean jib
-kubectl create -f kubernetes.yml
--- redeploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-./gradlew jib
-kubectl patch deployment example-service -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
-kubectl patch deployment example-client -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
