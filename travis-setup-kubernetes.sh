1c9c88c9bd2adc1799a57531d6e6d4ae951caf38
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2019-03-06 04:29:32 +0100

    Wait for service pod to be up and running

--- travis-setup-kubernetes.sh
+++ travis-setup-kubernetes.sh
@@ -29,8 +29,7 @@ kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin
 kubectl create -f kubernetes.yml
 
 # Wait for Service pod to be up and ready
-SERVICE_POD="$(kubectl get pods | grep "example-service" | awk 'FNR <= 1 { print $1 }')"
-JSONPATH='{.status.conditions[?(@.type=="Ready")].status}'; until kubectl get pod $SERVICE_POD -o jsonpath="$JSONPATH" 2>&1 | grep -q "True"; do sleep 1; done
+kubectl wait --for=condition=Ready pod/`kubectl get pods | grep "example-service" | awk 'FNR <= 1 { print $1 }'`
 
 # Expose the service's port locally
 kubectl port-forward `kubectl get pods | grep "example-service" | awk 'FNR <= 1 { print $1 }'` 8081:8081 &
