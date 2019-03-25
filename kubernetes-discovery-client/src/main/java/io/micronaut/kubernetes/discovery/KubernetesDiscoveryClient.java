5d529f0b972a260911c6a046eaefafbea1d47053
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2019-03-25 08:12:48 +0100

    Logging improvements

--- kubernetes-discovery-client/src/main/java/io/micronaut/kubernetes/discovery/KubernetesDiscoveryClient.java
+++ kubernetes-discovery-client/src/main/java/io/micronaut/kubernetes/discovery/KubernetesDiscoveryClient.java
@@ -19,6 +19,7 @@ package io.micronaut.kubernetes.discovery;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.context.env.Environment;
 import io.micronaut.core.async.publisher.Publishers;
+import io.micronaut.core.convert.value.ConvertibleValues;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.discovery.DiscoveryClient;
 import io.micronaut.discovery.ServiceInstance;
@@ -30,11 +31,12 @@ import io.micronaut.kubernetes.client.v1.endpoints.Endpoints;
 import io.micronaut.kubernetes.client.v1.services.ServiceList;
 import io.reactivex.Flowable;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
@@ -55,6 +57,8 @@ import static io.micronaut.kubernetes.client.v1.KubernetesClient.SERVICE_ID;
 public class KubernetesDiscoveryClient implements DiscoveryClient {
 
     public static final String KUBERNETES_URI = "https://kubernetes";
+    protected static final Logger LOG = LoggerFactory.getLogger(KubernetesDiscoveryClient.class);
+
     private final KubernetesClient client;
     private final KubernetesDiscoveryConfiguration configuration;
 
@@ -71,14 +75,12 @@ public class KubernetesDiscoveryClient implements DiscoveryClient {
     @Override
     public Publisher<List<ServiceInstance>> getInstances(String serviceId) {
         if (SERVICE_ID.equals(serviceId)) {
-            return Publishers.just(
-                    Collections.singletonList(ServiceInstance.of(SERVICE_ID,
-                            URI.create(KUBERNETES_URI)))
-            );
+            return Publishers.just(Collections.singletonList(ServiceInstance.of(SERVICE_ID, URI.create(KUBERNETES_URI))));
         } else {
             AtomicReference<Metadata> metadata = new AtomicReference<>();
-            return Flowable.fromPublisher(client.getEndpoints(configuration.getKubernetesConfiguration().getNamespace(), serviceId))
-                    .doOnError(Throwable::printStackTrace)
+            String namespace = configuration.getKubernetesConfiguration().getNamespace();
+            return Flowable.fromPublisher(client.getEndpoints(namespace, serviceId))
+                    .doOnError(throwable -> LOG.error("Error while trying to get Kubernetes Endpoints for the service=[" + serviceId + "] in the namespace=[" + namespace + "]", throwable))
                     .doOnNext(endpoints -> metadata.set(endpoints.getMetadata()))
                     .flatMapIterable(Endpoints::getSubsets)
                     .map(subset -> subset
@@ -93,7 +95,13 @@ public class KubernetesDiscoveryClient implements DiscoveryClient {
         boolean isSecure = port.isSecure() || metadata.isSecure();
         String scheme = isSecure ? "https://" : "http://";
         URI uri = URI.create(scheme + address.getIp().getHostAddress() + ":" + port.getPort());
-        return ServiceInstance.of(serviceId, uri);
+        if (LOG.isTraceEnabled()) {
+            LOG.trace("ServiceInstance for serviceId={} has URI={}", serviceId, uri.toString());
+        }
+        return ServiceInstance
+                .builder(serviceId, uri)
+                .metadata(metadata.getLabels())
+                .build();
     }
 
     /**
@@ -103,7 +111,7 @@ public class KubernetesDiscoveryClient implements DiscoveryClient {
     @Override
     public Publisher<List<String>> getServiceIds() {
         return Flowable.fromPublisher(client.listServices())
-                .doOnError(Throwable::printStackTrace)
+                .doOnError(throwable -> LOG.error("Error while trying to list all Kubernetes Services", throwable))
                 .flatMapIterable(ServiceList::getItems)
                 .map(service -> service.getMetadata().getName())
                 .toList()
@@ -112,11 +120,11 @@ public class KubernetesDiscoveryClient implements DiscoveryClient {
 
     @Override
     public String getDescription() {
-        return null;
+        return SERVICE_ID;
     }
 
     @Override
     public void close() throws IOException {
-
+        //no op
     }
 }
