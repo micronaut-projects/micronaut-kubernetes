aa5b604d91d234520d5abca2735c4d5404b7cdee
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2019-06-28 14:16:59 +0200

    Adding script to cleanup the Kubernetes cluster

---
+++ cleanup-kubernetes.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+killall -9 kubectl
+kubectl delete -f k8s-auth.yml
+kubectl delete -f kubernetes.yml
+kubectl delete configmaps game-config-properties
+kubectl delete configmaps game-config-yml
+kubectl delete configmaps game-config-json
+kubectl delete configmaps literal-config
+kubectl delete secret test-secret
