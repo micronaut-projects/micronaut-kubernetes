4f3c79b4101fe36d4e653a6f17ae49091c7f639d
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2019-07-26 12:56:22 +0200

    Exposing ports in Travis

--- travis-setup-kubernetes.sh
+++ travis-setup-kubernetes.sh
@@ -30,14 +30,17 @@ kubectl create -f k8s-auth.yml
 kubectl create -f kubernetes.yml
 ./create-config-maps-and-secret.sh
 # Wait for pods to be up and ready
-sleep 20
-SERVICE_POD="$(kubectl get pods | grep "example-service" | awk 'FNR <= 1 { print $1 }')"
-CLIENT_POD="$(kubectl get pods | grep "example-client" | awk 'FNR <= 1 { print $1 }')"
-kubectl wait --for=condition=Ready pod/$SERVICE_POD
+SERVICE_POD_1="$(kubectl get pods | grep "example-service" | awk 'FNR <= 1 { print $1 }')"
+SERVICE_POD_2="$(kubectl get pods | grep "example-service" | awk 'FNR > 1 { print $1 }')"kubectl wait --for=condition=Ready pod/$SERVICE_POD
+kubectl wait --for=condition=Ready pod/$SERVICE_POD_1
 kubectl wait --for=condition=Ready pod/$CLIENT_POD
-
-# Expose the client's port locally
+kubectl wait --for=condition=Ready pod/$SERVICE_POD_2
+# Expose ports locally
+kubectl port-forward $SERVICE_POD_1 9999:8081 &
+kubectl port-forward $SERVICE_POD_1 5004:5004 &
+kubectl port-forward $SERVICE_POD_2 9998:8081 &
 kubectl port-forward $CLIENT_POD 8888:8082 &
+kubectl port-forward $CLIENT_POD 5005:5005 &
 
 kubectl logs -f $SERVICE_POD &
 kubectl logs -f $CLIENT_POD &
