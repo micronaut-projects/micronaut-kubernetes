42877b727b5a51af27caa4a10724de2656ae00e4
Max Kuchin <mkuchin@gmail.com>
2020-07-21 15:33:58 +1000

    missing import

--- kubernetes-discovery-client/src/main/java/io/micronaut/kubernetes/client/v1/InetAddressDeserializer.java
+++ kubernetes-discovery-client/src/main/java/io/micronaut/kubernetes/client/v1/InetAddressDeserializer.java
@@ -20,6 +20,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import io.micronaut.core.annotation.Introspected;
 
 import java.io.IOException;
 import java.net.InetAddress;
