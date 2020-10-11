e8e4210102221ba58ffeee76d497abfabffee7a3
Pavol Gressa <pavol.gressa@oracle.com>
2020-10-11 20:18:09 +0200

    Add various K8s version support to setup.sh

--- setup.sh
+++ setup.sh
@@ -3,20 +3,45 @@ set -x
 
 sudo apt-get update
 
-KIND_VERSION=v0.9.0
+KIND_VERSION="v0.9.0"
+KUBECTL_VERSION=""
+KIND_NODE_IMAGE_VERSION=""
+K8S_VERSION=${K8S_VERSION:=1_19}
+
+case $K8S_VERSION in
+  1_16)
+    KIND_NODE_IMAGE_VERSION="kindest/node:v1.16.15@sha256:a89c771f7de234e6547d43695c7ab047809ffc71a0c3b65aa54eda051c45ed20"
+    KUBECTL_VERSION="v1.16.15"
+    ;;
+
+  1_17)
+    KIND_NODE_IMAGE_VERSION="kindest/node:v1.17.11@sha256:5240a7a2c34bf241afb54ac05669f8a46661912eab05705d660971eeb12f6555"
+    KUBECTL_VERSION="v1.17.12"
+    ;;
+
+  1_18)
+    KIND_NODE_IMAGE_VERSION="kindest/node:v1.18.8@sha256:f4bcc97a0ad6e7abaf3f643d890add7efe6ee4ab90baeb374b4f41a4c95567eb"
+    KUBECTL_VERSION="v1.18.9"
+    ;;
+
+  1_19 | *)
+    KIND_NODE_IMAGE_VERSION="kindest/node:v1.19.1@sha256:98cf5288864662e37115e362b23e4369c8c4a408f99cbc06e58ac30ddc721600"
+    KUBECTL_VERSION="v1.19.2"
+    ;;
+esac
+
+echo "KIND_VERSION = $KIND_VERSION"
+echo "KUBECTL_VERSION = $KUBECTL_VERSION"
+echo "KIND_NODE_IMAGE_VERSION = $KIND_NODE_IMAGE_VERSION"
 
 # Download and install kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
 # Download and install kind
 curl -Lo ./kind "https://kind.sigs.k8s.io/dl/${KIND_VERSION}/kind-$(uname)-amd64" && chmod +x ./kind && sudo mv kind /usr/local/bin/
 
 # Create a cluster
-kind create cluster --wait 5m
-
-# Configure kubectl
-cp $(kind get kubeconfig-path) $HOME/.kube/config
-
+kind create cluster --image ${KIND_NODE_IMAGE_VERSION} --wait 5m
 kubectl cluster-info
 kubectl version
 
