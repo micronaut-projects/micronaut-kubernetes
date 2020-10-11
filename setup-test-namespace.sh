6626bf5705c654282cb4a68e360a5f6e43b07ec3
Pavol Gressa <pavol.gressa@oracle.com>
2020-10-11 19:31:36 +0200

    move resources to test resources

--- setup-test-namespace.sh
+++ setup-test-namespace.sh
@@ -27,9 +27,9 @@ kubectl create role service-discoverer --verb=get,list,watch --resource=services
 kubectl create rolebinding default-service-discoverer --role service-discoverer --serviceaccount=${NAMESPACE}:default
 
 # Create secrets and config maps
-kubectl create configmap game-config-properties --from-file=kubernetes-discovery-client/src/k8s/game.properties
-kubectl create configmap game-config-yml --from-file=kubernetes-discovery-client/src/k8s/game.yml
-kubectl create configmap game-config-json --from-file=kubernetes-discovery-client/src/k8s/game.json
+kubectl create configmap game-config-properties --from-file=kubernetes-discovery-client/src/test/resources/k8s/game.properties
+kubectl create configmap game-config-yml --from-file=kubernetes-discovery-client/src/test/resources/k8s/game.yml
+kubectl create configmap game-config-json --from-file=kubernetes-discovery-client/src/test/resources/k8s/game.json
 kubectl create configmap literal-config --from-literal=special.how=very --from-literal=special.type=charm
 kubectl create secret generic test-secret --from-literal=username='my-app' --from-literal=password='39528$vdg7Jb'
 kubectl create secret generic another-secret --from-literal=secretProperty='secretValue'
@@ -40,97 +40,16 @@ kubectl label configmap literal-config app=game
 kubectl label secret another-secret app=game
 
 # Create and expose deployment - example service
-cat <<EOF | kubectl apply -f -
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: example-service
-spec:
-  selector:
-    matchLabels:
-      app: example-service
-  replicas: 2
-  template:
-    metadata:
-      labels:
-        app: example-service
-    spec:
-      containers:
-        - name: example-service
-          image: micronaut-kubernetes-example-service
-          imagePullPolicy: "Never"
-          volumeMounts:
-            - name: secrets
-              mountPath: "/etc/example-service/secrets"
-              readOnly: true
-          env:
-            - name: JAVA_TOOL_OPTIONS
-              value: "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5004"
-          ports:
-            - name: http
-              containerPort: 8081
-            - name: "jvm-debug"
-              containerPort: 5004
-      volumes:
-        - name: secrets
-          secret:
-            secretName: mounted-secret
-            defaultMode: 256
-EOF
+kubectl apply -f kubernetes-discovery-client/src/test/resources/k8s/example-service-deployment.yml
 kubectl expose  deployment example-service --type LoadBalancer --port 8081 --target-port 8081 --labels foo=bar
 
 # Create and expose deployment - example client
-cat <<EOF | kubectl apply -f -
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: example-client
-spec:
-  selector:
-    matchLabels:
-      app: example-client
-  template:
-    metadata:
-      labels:
-        app: example-client
-    spec:
-      containers:
-        - name: example-client
-          image: micronaut-kubernetes-example-client
-          imagePullPolicy: "Never"
-          env:
-            - name: JAVA_TOOL_OPTIONS
-              value: "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
-          ports:
-            - name: http
-              containerPort: 8082
-            - name: "jvm-debug"
-              containerPort: 5005
-EOF
+kubectl apply -f kubernetes-discovery-client/src/test/resources/k8s/example-client-deployment.yml
 kubectl expose deployment example-client --type LoadBalancer --port 8082 --target-port 8082
 
 # Create and expose deployment - secure deployment
-cat <<EOF | kubectl apply -f -
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: secure-deployment
-spec:
-  selector:
-    matchLabels:
-      app: secure-deployment
-  replicas: 1
-  template:
-    metadata:
-      labels:
-        app: secure-deployment
-    spec:
-      containers:
-        - name: secure-deployment
-          image: nginx
-          ports:
-            - containerPort: 80
-EOF
+kubectl apply -f kubernetes-discovery-client/src/test/resources/k8s/secure-deployment.yml
+
 # with port name
 kubectl expose deployment secure-deployment --name secure-service-port-name --type NodePort --port 1234
 kubectl patch svc secure-service-port-name --type='json' -p='[{"op": "replace", "path": "/spec/ports/0/name", "value": "https" }]'
