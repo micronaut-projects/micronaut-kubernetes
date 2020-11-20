a6223328b9227058e4dad72b7d89a233fb74b2d2
Pavol Gressa <1838702+pgressa@users.noreply.github.com>
2020-11-20 14:27:37 +0100

    Use MicronautTest extension instead of deprecated one (#212)

--- kubernetes-discovery-client/src/test/groovy/io/micronaut/kubernetes/configuration/KubernetesConfigurationClientSpec.groovy
+++ kubernetes-discovery-client/src/test/groovy/io/micronaut/kubernetes/configuration/KubernetesConfigurationClientSpec.groovy
@@ -2,13 +2,12 @@ package io.micronaut.kubernetes.configuration
 
 import groovy.util.logging.Slf4j
 import io.micronaut.context.ApplicationContext
-import io.micronaut.context.annotation.Property
 import io.micronaut.context.env.Environment
 import io.micronaut.context.env.EnvironmentPropertySource
 import io.micronaut.context.env.PropertySource
 import io.micronaut.kubernetes.test.KubectlCommands
 import io.micronaut.kubernetes.test.TestUtils
-import io.micronaut.test.annotation.MicronautTest
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import io.reactivex.Flowable
 import spock.lang.Requires
 import spock.lang.Specification
