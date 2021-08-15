7e7eb8098bf4183b56ce6d8b5dc5105f38d62b01
Pavol Gressa <1838702+pgressa@users.noreply.github.com>
2021-08-15 12:44:01 +0200

    feat: RxJava and Reactor support for official k8s client (#314)

---
+++ kubernetes-client-reactor/src/main/java/io/micronaut/kubernetes/client/reactor/AsyncCallbackSink.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017-2021 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.kubernetes.client.reactor;
+
+import io.kubernetes.client.openapi.ApiCallback;
+import io.kubernetes.client.openapi.ApiException;
+import io.micronaut.kubernetes.client.Apis;
+import reactor.core.publisher.MonoSink;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Bridges the {@link ApiCallback} interface to an Reactor {@link MonoSink}.
+ *
+ * @author Pavol Gressa
+ * @since 3.0
+ */
+@Apis(kind = Apis.Kind.REACTOR)
+public class AsyncCallbackSink<T> implements ApiCallback<T> {
+    private final MonoSink<T> monoSink;
+
+    public AsyncCallbackSink(MonoSink<T> monoSink) {
+        this.monoSink = monoSink;
+    }
+
+    @Override
+    public void onFailure(ApiException e, int statusCode, Map<String, List<String>> responseHeaders) {
+        monoSink.error(e);
+    }
+
+    @Override
+    public void onSuccess(T result, int statusCode, Map<String, List<String>> responseHeaders) {
+        monoSink.success(result);
+    }
+
+    @Override
+    public void onUploadProgress(long bytesWritten, long contentLength, boolean done) {
+        // no-op
+    }
+
+    @Override
+    public void onDownloadProgress(long bytesRead, long contentLength, boolean done) {
+        // no-op
+    }
+}
--- kubernetes-client/src/main/java/io/micronaut/kubernetes/client/Apis.java
+++ kubernetes-client/src/main/java/io/micronaut/kubernetes/client/Apis.java
@@ -15,14 +15,179 @@
  */
 package io.micronaut.kubernetes.client;
 
+import io.kubernetes.client.openapi.apis.AdmissionregistrationApi;
+import io.kubernetes.client.openapi.apis.AdmissionregistrationV1Api;
+import io.kubernetes.client.openapi.apis.AdmissionregistrationV1beta1Api;
+import io.kubernetes.client.openapi.apis.ApiextensionsApi;
+import io.kubernetes.client.openapi.apis.ApiextensionsV1Api;
+import io.kubernetes.client.openapi.apis.ApiextensionsV1beta1Api;
+import io.kubernetes.client.openapi.apis.ApiregistrationApi;
+import io.kubernetes.client.openapi.apis.ApiregistrationV1Api;
+import io.kubernetes.client.openapi.apis.ApiregistrationV1beta1Api;
+import io.kubernetes.client.openapi.apis.ApisApi;
+import io.kubernetes.client.openapi.apis.AppsApi;
+import io.kubernetes.client.openapi.apis.AppsV1Api;
+import io.kubernetes.client.openapi.apis.AuthenticationApi;
+import io.kubernetes.client.openapi.apis.AuthenticationV1Api;
+import io.kubernetes.client.openapi.apis.AuthenticationV1beta1Api;
+import io.kubernetes.client.openapi.apis.AuthorizationApi;
+import io.kubernetes.client.openapi.apis.AuthorizationV1Api;
+import io.kubernetes.client.openapi.apis.AuthorizationV1beta1Api;
+import io.kubernetes.client.openapi.apis.AutoscalingApi;
+import io.kubernetes.client.openapi.apis.AutoscalingV1Api;
+import io.kubernetes.client.openapi.apis.AutoscalingV2beta1Api;
+import io.kubernetes.client.openapi.apis.AutoscalingV2beta2Api;
+import io.kubernetes.client.openapi.apis.BatchApi;
+import io.kubernetes.client.openapi.apis.BatchV1Api;
+import io.kubernetes.client.openapi.apis.BatchV1beta1Api;
+import io.kubernetes.client.openapi.apis.BatchV2alpha1Api;
+import io.kubernetes.client.openapi.apis.CertificatesApi;
+import io.kubernetes.client.openapi.apis.CertificatesV1Api;
+import io.kubernetes.client.openapi.apis.CertificatesV1beta1Api;
+import io.kubernetes.client.openapi.apis.CoordinationApi;
+import io.kubernetes.client.openapi.apis.CoordinationV1Api;
+import io.kubernetes.client.openapi.apis.CoordinationV1beta1Api;
+import io.kubernetes.client.openapi.apis.CoreApi;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.apis.CustomObjectsApi;
+import io.kubernetes.client.openapi.apis.DiscoveryApi;
+import io.kubernetes.client.openapi.apis.DiscoveryV1beta1Api;
+import io.kubernetes.client.openapi.apis.EventsApi;
+import io.kubernetes.client.openapi.apis.EventsV1Api;
+import io.kubernetes.client.openapi.apis.EventsV1beta1Api;
+import io.kubernetes.client.openapi.apis.ExtensionsApi;
+import io.kubernetes.client.openapi.apis.ExtensionsV1beta1Api;
+import io.kubernetes.client.openapi.apis.FlowcontrolApiserverApi;
+import io.kubernetes.client.openapi.apis.FlowcontrolApiserverV1alpha1Api;
+import io.kubernetes.client.openapi.apis.FlowcontrolApiserverV1beta1Api;
+import io.kubernetes.client.openapi.apis.InternalApiserverApi;
+import io.kubernetes.client.openapi.apis.InternalApiserverV1alpha1Api;
+import io.kubernetes.client.openapi.apis.LogsApi;
+import io.kubernetes.client.openapi.apis.NetworkingApi;
+import io.kubernetes.client.openapi.apis.NetworkingV1Api;
+import io.kubernetes.client.openapi.apis.NetworkingV1beta1Api;
+import io.kubernetes.client.openapi.apis.NodeApi;
+import io.kubernetes.client.openapi.apis.NodeV1Api;
+import io.kubernetes.client.openapi.apis.NodeV1alpha1Api;
+import io.kubernetes.client.openapi.apis.NodeV1beta1Api;
+import io.kubernetes.client.openapi.apis.OpenidApi;
+import io.kubernetes.client.openapi.apis.PolicyApi;
+import io.kubernetes.client.openapi.apis.PolicyV1beta1Api;
+import io.kubernetes.client.openapi.apis.RbacAuthorizationApi;
+import io.kubernetes.client.openapi.apis.RbacAuthorizationV1Api;
+import io.kubernetes.client.openapi.apis.RbacAuthorizationV1alpha1Api;
+import io.kubernetes.client.openapi.apis.RbacAuthorizationV1beta1Api;
+import io.kubernetes.client.openapi.apis.SchedulingApi;
+import io.kubernetes.client.openapi.apis.SchedulingV1Api;
+import io.kubernetes.client.openapi.apis.SchedulingV1alpha1Api;
+import io.kubernetes.client.openapi.apis.SchedulingV1beta1Api;
+import io.kubernetes.client.openapi.apis.StorageApi;
+import io.kubernetes.client.openapi.apis.StorageV1Api;
+import io.kubernetes.client.openapi.apis.StorageV1alpha1Api;
+import io.kubernetes.client.openapi.apis.StorageV1beta1Api;
+import io.kubernetes.client.openapi.apis.VersionApi;
+import io.kubernetes.client.openapi.apis.WellKnownApi;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Internal Annotation to trigger the creation of Api clients.
- * @since 2.2
+ * Internal annotation to trigger the creation of Api clients.
+ *
+ * @since 3.0
  */
-@Retention(value = RetentionPolicy.RUNTIME)
+@Retention(value = RetentionPolicy.SOURCE)
 public @interface Apis {
-    Class<?>[] value();
+
+    /**
+     * @return The array of client API objects to generate.
+     */
+    Class<?>[] value() default {
+            AdmissionregistrationApi.class,
+            AdmissionregistrationV1Api.class,
+            AdmissionregistrationV1beta1Api.class,
+            ApiextensionsApi.class,
+            ApiextensionsV1Api.class,
+            ApiextensionsV1beta1Api.class,
+            ApiregistrationApi.class,
+            ApiregistrationV1Api.class,
+            ApiregistrationV1beta1Api.class,
+            ApisApi.class,
+            AppsApi.class,
+            AppsV1Api.class,
+            AuthenticationApi.class,
+            AuthenticationV1Api.class,
+            AuthenticationV1beta1Api.class,
+            AuthorizationApi.class,
+            AuthorizationV1Api.class,
+            AuthorizationV1beta1Api.class,
+            AutoscalingApi.class,
+            AutoscalingV1Api.class,
+            AutoscalingV2beta1Api.class,
+            AutoscalingV2beta2Api.class,
+            BatchApi.class,
+            BatchV1Api.class,
+            BatchV1beta1Api.class,
+            BatchV2alpha1Api.class,
+            CertificatesApi.class,
+            CertificatesV1Api.class,
+            CertificatesV1beta1Api.class,
+            CoordinationApi.class,
+            CoordinationV1Api.class,
+            CoordinationV1beta1Api.class,
+            CoreApi.class,
+            CoreV1Api.class,
+            CustomObjectsApi.class,
+            DiscoveryApi.class,
+            DiscoveryV1beta1Api.class,
+            EventsApi.class,
+            EventsV1Api.class,
+            EventsV1beta1Api.class,
+            ExtensionsApi.class,
+            ExtensionsV1beta1Api.class,
+            FlowcontrolApiserverApi.class,
+            FlowcontrolApiserverV1alpha1Api.class,
+            FlowcontrolApiserverV1beta1Api.class,
+            InternalApiserverApi.class,
+            InternalApiserverV1alpha1Api.class,
+            LogsApi.class,
+            NetworkingApi.class,
+            NetworkingV1Api.class,
+            NetworkingV1beta1Api.class,
+            NodeApi.class,
+            NodeV1Api.class,
+            NodeV1alpha1Api.class,
+            NodeV1beta1Api.class,
+            OpenidApi.class,
+            PolicyApi.class,
+            PolicyV1beta1Api.class,
+            RbacAuthorizationApi.class,
+            RbacAuthorizationV1Api.class,
+            RbacAuthorizationV1alpha1Api.class,
+            RbacAuthorizationV1beta1Api.class,
+            SchedulingApi.class,
+            SchedulingV1Api.class,
+            SchedulingV1alpha1Api.class,
+            SchedulingV1beta1Api.class,
+            StorageApi.class,
+            StorageV1Api.class,
+            StorageV1alpha1Api.class,
+            StorageV1beta1Api.class,
+            VersionApi.class,
+            WellKnownApi.class
+    };
+
+    /**
+     * @return The type of client to generate.
+     */
+    Kind kind() default Kind.ASYNC;
+
+    /**
+     * the type of client to generate.
+     */
+    enum Kind {
+        ASYNC,
+        REACTOR,
+        RXJAVA2
+    }
 }
