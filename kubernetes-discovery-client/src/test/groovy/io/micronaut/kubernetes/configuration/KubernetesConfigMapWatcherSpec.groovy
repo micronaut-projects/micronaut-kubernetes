8194e1b9b776608d33fd3039c3bf7e859d77aab9
Pavol Gressa <pavol.gressa@oracle.com>
2021-09-09 10:20:27 +0200

    test now needs kubectl because of the informer

--- kubernetes-discovery-client/src/test/groovy/io/micronaut/kubernetes/configuration/KubernetesConfigMapWatcherSpec.groovy
+++ kubernetes-discovery-client/src/test/groovy/io/micronaut/kubernetes/configuration/KubernetesConfigMapWatcherSpec.groovy
@@ -1,12 +1,19 @@
 package io.micronaut.kubernetes.configuration
 
 import io.micronaut.context.ApplicationContext
+import io.micronaut.context.annotation.Property
 import io.micronaut.context.env.Environment
+import io.micronaut.kubernetes.test.TestUtils
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Requires
 import spock.lang.Specification
 
+@MicronautTest(environments = [Environment.KUBERNETES])
+@Requires({ TestUtils.kubernetesApiAvailable() })
+@Property(name = "spec.reuseNamespace", value = "false")
 class KubernetesConfigMapWatcherSpec extends Specification {
 
-    void "KubernetesConfigMapWatcher exists by default"() {
+    void "KubernetesConfigMapWatcher exists when informer is enabled"() {
         given:
         ApplicationContext applicationContext = ApplicationContext.run(Environment.KUBERNETES)
 
