a3ebb82b3061b58234f6e1e86f00819c45df8bd2
Nemanja Mikic <nemanja.mikic@oracle.com>
2023-03-21 14:49:36 +0100

    experiment

--- kubernetes-client/src/main/java/io/micronaut/kubernetes/client/OkHttpClientRetryConnectionRefused.java
+++ kubernetes-client/src/main/java/io/micronaut/kubernetes/client/OkHttpClientRetryConnectionRefused.java
@@ -4,26 +4,34 @@ import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.ConnectException;
 
 public class OkHttpClientRetryConnectionRefused implements Interceptor {
 
+    private static final Logger LOG = LoggerFactory.getLogger(OkHttpClientRetryConnectionRefused.class);
+
     static int RETRY_CONNECTION_REFUSE_TIMES = 5;
+
     @NotNull
     @Override
     public Response intercept(@NotNull Chain chain) throws IOException {
         Request request = chain.request();
         int tryCount = 0;
-        while (tryCount < RETRY_CONNECTION_REFUSE_TIMES) {
+        while (tryCount < (RETRY_CONNECTION_REFUSE_TIMES - 1)) {
             tryCount ++;
             try {
                 return chain.proceed(request);
             } catch (ConnectException e) {
                 if (e.getMessage().startsWith("Connection refused")) {
-                    if (tryCount == RETRY_CONNECTION_REFUSE_TIMES -1) {
-                        throw e;
+                    try {
+                        LOG.debug("Waiting 1s to try again");
+                        Thread.sleep( 1000);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
                     }
                 } else {
                     throw e;
@@ -32,5 +40,6 @@ public class OkHttpClientRetryConnectionRefused implements Interceptor {
                 throw e;
             }
         }
+        return chain.proceed(request);
     }
 }
