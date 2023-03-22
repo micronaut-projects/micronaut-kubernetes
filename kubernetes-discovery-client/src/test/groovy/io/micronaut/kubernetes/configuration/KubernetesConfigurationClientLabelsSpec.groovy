a4c4b4cfd8fe25957f25369c9831a43577300872
Nemanja Mikic <nemanja.mikic@oracle.com>
2023-03-22 11:49:31 +0100

    experiment

--- kubernetes-discovery-client/src/test/groovy/io/micronaut/kubernetes/configuration/KubernetesConfigurationClientLabelsSpec.groovy
+++ kubernetes-discovery-client/src/test/groovy/io/micronaut/kubernetes/configuration/KubernetesConfigurationClientLabelsSpec.groovy
@@ -14,7 +14,7 @@ import spock.lang.Requires
 
 @MicronautTest(environments = [Environment.KUBERNETES])
 @Requires({ TestUtils.kubernetesApiAvailable() })
-@Property(name = "kubernetes.client.namespace", value = "Kubernetes-configuration-client-labels-spec")
+@Property(name = "kubernetes.client.namespace", value = "kubernetes-configuration-client-labels-spec")
 @Property(name = "spec.reuseNamespace", value = "false")
 class KubernetesConfigurationClientLabelsSpec extends KubernetesSpecification {
 
