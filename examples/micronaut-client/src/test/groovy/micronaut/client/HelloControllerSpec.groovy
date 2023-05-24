439d082fdb02da108b4e1801724706807e646e93
Nemanja Mikic <nemanja.mikic@oracle.com>
2023-05-24 18:15:24 +0200

    fix test with changing MediaType

--- examples/micronaut-client/src/test/groovy/micronaut/client/HelloControllerSpec.groovy
+++ examples/micronaut-client/src/test/groovy/micronaut/client/HelloControllerSpec.groovy
@@ -2,7 +2,7 @@ package micronaut.client
 
 import io.micronaut.context.annotation.Property
 import io.micronaut.context.env.Environment
-    import io.micronaut.http.MediaType
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.client.annotation.Client
