849157e2cb46c9edc8ce18eebab48a9405650471
Tim Yates <tim.yates@gmail.com>
2024-04-03 05:50:51 +0100

    Update the build so it works with Java 21 (#687)
    * Re-introduce sleep to stop thrashing the CPU
    
    * Gradle 8.7
    
    * Fix non-sensical dependency
    
    * Remove compilation fork
    
    * Update common files
    
    * Update plugin versions, remove groovy version
    
    * Limit to 17 for now
    
    * Try to see what's going on in informer test
    
    * Switch to Logger and give more memory 🤞
    
    * Remove add-opens
    
    * Try to add more logging and apply the common plugin
    
    * Try to see what's going on in informer test take 2
    
    * Remove debug, fix test for local
    
    * Remove memory limit, and try to stop capturing test output in scans (as there's a lot)
    
    * We do need SystemLambda after all
    
    * Under the assumption that we're logging too much, reduce the logging
    
    * Try to disable test distribution
    
    As were seeing an error com.gradle.enterprise.testdistribution.common.a.i.b(SourceFile:228)
    
    * Try in just the kubernetes-discovery-client tests
    
    * Fix add-opens -- it's needed for systemlambda
    
    * Try to disable PTS to see if that helps
    
    * Disable logging for kubernetes-discovery-client tests
    
    * Disable logging with OFF, not removal
    
    * Bump the gradle plugin, and fix docker jre for examples
    
    * Use prefixes for projects
    
    * More CI flakiness
    
    * Tell sonar to ignore the examples
    
    * Fix Graal pipelines
    
    * Sonar
    
    ---------
    
    Co-authored-by: micronaut-build <micronaut-build-account@grails.org>

---
+++ examples/example-kubernetes-informer/src/test/groovy/micronaut/informer/SecretInformerControllerSpec.groovy
@@ -0,0 +1,159 @@
+package micronaut.informer
+
+import io.fabric8.kubernetes.api.model.ContainerBuilder
+import io.fabric8.kubernetes.api.model.ContainerPortBuilder
+import io.fabric8.kubernetes.api.model.HTTPGetActionBuilder
+import io.fabric8.kubernetes.api.model.IntOrString
+import io.fabric8.kubernetes.api.model.LabelSelectorBuilder
+import io.fabric8.kubernetes.api.model.ObjectMetaBuilder
+import io.fabric8.kubernetes.api.model.PodSpecBuilder
+import io.fabric8.kubernetes.api.model.PodTemplateSpecBuilder
+import io.fabric8.kubernetes.api.model.ProbeBuilder
+import io.fabric8.kubernetes.api.model.ServicePortBuilder
+import io.fabric8.kubernetes.api.model.ServiceSpecBuilder
+import io.fabric8.kubernetes.api.model.apps.DeploymentBuilder
+import io.fabric8.kubernetes.api.model.apps.DeploymentSpecBuilder
+import io.kubernetes.client.openapi.models.V1Secret
+import io.micronaut.context.annotation.Property
+import io.micronaut.context.annotation.Requires as MicronautRequires
+import io.micronaut.context.env.Environment
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.kubernetes.test.KubernetesSpecification
+import io.micronaut.kubernetes.test.TestUtils
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+import spock.lang.Requires
+import spock.lang.Shared
+import spock.util.concurrent.PollingConditions
+
+import java.util.concurrent.TimeUnit
+
+@MicronautTest(environments = [Environment.KUBERNETES], startApplication = false)
+@Property(name = "spec.name", value = "SecretInformerControllerSpec")
+@Property(name = "spec.reuseNamespace", value = "false")
+@Property(name = "kubernetes.client.namespace", value = "micronaut-example-informer")
+@Requires({ TestUtils.kubernetesApiAvailable() })
+class SecretInformerControllerSpec extends KubernetesSpecification {
+
+    @Inject
+    @Shared
+    TestClient testClient
+
+    @Override
+    def setupFixture(String namespace) {
+        createNamespaceSafe(namespace)
+        createBaseResources(namespace)
+        def client = operations.getClient(namespace)
+        def informerDeployment = client.apps().deployments().createOrReplace(
+                new DeploymentBuilder()
+                        .withMetadata(new ObjectMetaBuilder()
+                                .withName("example-informer")
+                                .build())
+                        .withSpec(new DeploymentSpecBuilder()
+                                .withSelector(new LabelSelectorBuilder()
+                                        .addToMatchLabels("app", "example-informer")
+                                        .build())
+                                .withReplicas(1)
+                                .withTemplate(new PodTemplateSpecBuilder()
+                                        .withMetadata(new ObjectMetaBuilder()
+                                                .withLabels(["app": "example-informer"])
+                                                .build())
+                                        .withSpec(new PodSpecBuilder()
+                                                .withContainers(new ContainerBuilder()
+                                                        .withName("informer")
+                                                        .withImage("micronaut-kubernetes-informer-example")
+                                                        .withImagePullPolicy("Never")
+                                                        .withPorts(new ContainerPortBuilder()
+                                                                .withName("http")
+                                                                .withContainerPort(8080)
+                                                                .build())
+                                                        .withLivenessProbe(new ProbeBuilder()
+                                                                .withHttpGet(new HTTPGetActionBuilder()
+                                                                        .withPath("/health/liveness")
+                                                                        .withPort(new IntOrString(8080))
+                                                                        .build())
+                                                                .withInitialDelaySeconds(1)
+                                                                .withPeriodSeconds(1)
+                                                                .withFailureThreshold(10)
+                                                                .build())
+                                                        .withReadinessProbe(new ProbeBuilder()
+                                                                .withHttpGet(new HTTPGetActionBuilder()
+                                                                        .withPath("/health/readiness")
+                                                                        .withPort(new IntOrString(8080))
+                                                                        .build())
+                                                                .withInitialDelaySeconds(1)
+                                                                .withPeriodSeconds(1)
+                                                                .withFailureThreshold(10)
+                                                                .build())
+                                                        .build())
+                                                .build())
+                                        .build())
+                                .build())
+                        .build())
+
+        client.apps().deployments().inNamespace(informerDeployment.getMetadata().getNamespace())
+                .withName(informerDeployment.getMetadata().getName()).waitUntilReady(250, TimeUnit.SECONDS)
+
+        operations.createService("example-informer", namespace,
+                new ServiceSpecBuilder()
+                        .withType("LoadBalancer")
+                        .withPorts(
+                                new ServicePortBuilder()
+                                        .withPort(8080)
+                                        .withTargetPort(new IntOrString(8080))
+                                        .build()
+                        )
+                        .withSelector(["app": "example-informer"])
+                        .build())
+
+        operations.portForwardService("example-informer", namespace, 8080, 8889)
+    }
+
+    void "test all"() {
+        expect:
+        testClient.all().metadata.name.findAll { !it.startsWith("default-token") }.size() == 3
+        testClient.secret("test-secret")
+        testClient.secret("test-secret").data.containsKey("username")
+    }
+
+    void "test secret"() {
+        given:
+        PollingConditions conditions = new PollingConditions(timeout: 30, delay: 2)
+        String secretName = "new-secret"
+
+        expect:
+        conditions.eventually {
+            testClient.secret("another-secret")
+        }
+
+        when:
+        operations.createSecret(secretName, namespace, ["foo": encodeSecret("bar")])
+
+        then:
+        conditions.eventually {
+            testClient.all().metadata.name.findAll { !it.startsWith("default-token") }.size() == 4
+            testClient.secret(secretName)
+        }
+
+        when:
+        operations.deleteSecret(secretName, namespace)
+
+        then:
+        conditions.eventually {
+            testClient.all().metadata.name.findAll { !it.startsWith("default-token") }.size() == 3
+        }
+    }
+
+    @Client("http://localhost:8889")
+    @MicronautRequires(property = "spec.name", value = "SecretInformerControllerSpec")
+    static interface TestClient {
+
+        @Get(uri = "/all", processes = MediaType.APPLICATION_JSON)
+        Collection<V1Secret> all()
+
+        @Get(uri = "/secret/{key}", processes = MediaType.APPLICATION_JSON)
+        V1Secret secret(String key)
+    }
+}
---
+++ examples/example-service/src/test/java/micronaut/service/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package micronaut.service;
+
+import io.micronaut.context.env.Environment;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.MediaType;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import jakarta.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@MicronautTest(environments = Environment.KUBERNETES)
+class HelloControllerTest {
+
+    @Inject
+    EmbeddedServer embeddedServer;
+
+    @Test
+    void testIndex() throws Exception {
+        try(HttpClient client = embeddedServer.getApplicationContext().createBean(HttpClient.class, embeddedServer.getURL())) {
+            HttpRequest<?> request = HttpRequest.GET("/hello/Alvaro").accept(MediaType.TEXT_PLAIN);
+            assertTrue(client.toBlocking().exchange(request, String.class).body().startsWith("Hello, Alvaro"));
+        }
+    }
+}
