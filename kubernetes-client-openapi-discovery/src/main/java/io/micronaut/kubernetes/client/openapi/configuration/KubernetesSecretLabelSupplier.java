3cc80b7de2bdbc083f5ad89a0034ea48feada712
Milenko Supic <milenko.supic@oracle.com>
2025-01-27 12:40:31 +0100

    WIP - configuration client impl

---
+++ kubernetes-client-openapi-discovery/src/main/java/io/micronaut/kubernetes/client/openapi/configuration/KubernetesSecretLabelSupplier.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2017-2025 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.kubernetes.client.openapi.configuration;
+
+import io.micronaut.context.annotation.Requires;
+import io.micronaut.context.env.Environment;
+import io.micronaut.kubernetes.client.openapi.KubernetesConfiguration;
+import io.micronaut.kubernetes.client.openapi.reactor.api.CoreV1ApiReactor;
+import jakarta.inject.Singleton;
+
+/**
+ * Based on configuration dynamically evaluates the label selector for secrets.
+ */
+@Singleton
+@Requires(env = Environment.KUBERNETES)
+final class KubernetesSecretLabelSupplier extends AbstractKubernetesConfigLabelSupplier {
+
+    KubernetesSecretLabelSupplier(CoreV1ApiReactor coreV1ApiReactor, KubernetesConfiguration configuration) {
+       super(coreV1ApiReactor, configuration, configuration.getSecrets());
+    }
+}
