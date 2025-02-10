25e8901191f071a25c4b257774a519aca57528d0
Nemanja Mikic <nemanja.mikic@oracle.com>
2025-02-10 16:49:01 +0100

    fix operation urils

--- kubernetes-client-openapi-discovery/src/test/groovy/io/micronaut/kubernetes/client/openapi/utils/OperationUtils.groovy
+++ kubernetes-client-openapi-discovery/src/test/groovy/io/micronaut/kubernetes/client/openapi/utils/OperationUtils.groovy
@@ -11,23 +11,23 @@ import io.micronaut.kubernetes.client.openapi.reactor.api.CoreV1ApiReactor
 class OperationUtils {
 
     static V1Namespace createNamespace(CoreV1ApiReactor api, V1Namespace namespace) {
-        api.createNamespace(namespace, null, null, null, null).block()
+        api.createNamespace(namespace, null, null, null, null).block().body()
     }
 
     static V1Endpoints createEndpoints(CoreV1ApiReactor api, String namespace, V1Endpoints endpoints) {
-        api.createNamespacedEndpoints(namespace, endpoints, null, null, null, null).block()
+        api.createNamespacedEndpoints(namespace, endpoints, null, null, null, null).block().body()
     }
 
     static V1Service createService(CoreV1ApiReactor api, String namespace, V1Service service) {
-        api.createNamespacedService(namespace, service, null, null, null, null).block()
+        api.createNamespacedService(namespace, service, null, null, null, null).block().body()
     }
 
     static V1ConfigMap createConfigMap(CoreV1ApiReactor api, String namespace, V1ConfigMap configMap) {
-        api.createNamespacedConfigMap(namespace, configMap, null, null, null, null).block()
+        api.createNamespacedConfigMap(namespace, configMap, null, null, null, null).block().body()
     }
 
     static V1ConfigMap replaceConfigMap(CoreV1ApiReactor api, String namespace, V1ConfigMap configMap) {
-        api.replaceNamespacedConfigMap(configMap.getMetadata().getName(), namespace, configMap, null, null, null, null).block()
+        api.replaceNamespacedConfigMap(configMap.getMetadata().getName(), namespace, configMap, null, null, null, null).block().body()
     }
 
     static void deleteConfigMap(CoreV1ApiReactor api, String namespace, String name) {
@@ -35,18 +35,18 @@ class OperationUtils {
     }
 
     static V1Pod createPod(CoreV1ApiReactor api, String namespace, V1Pod pod) {
-        api.createNamespacedPod(namespace, pod, null, null, null, null).block()
+        api.createNamespacedPod(namespace, pod, null, null, null, null).block().body()
     }
 
     static V1Secret createSecret(CoreV1ApiReactor api, String namespace, V1Secret secret) {
-        api.createNamespacedSecret(namespace, secret, null, null, null, null).block()
+        api.createNamespacedSecret(namespace, secret, null, null, null, null).block().body()
     }
 
     static V1Secret replaceSecret(CoreV1ApiReactor api, String namespace, V1Secret secret) {
-        api.replaceNamespacedSecret(secret.getMetadata().getName(), namespace, secret, null, null, null, null).block()
+        api.replaceNamespacedSecret(secret.getMetadata().getName(), namespace, secret, null, null, null, null).block().body()
     }
 
     static void deleteSecret(CoreV1ApiReactor api, String namespace, String name) {
-        api.deleteNamespacedSecret(name, namespace, null, null, null, null, null, null).block()
+        api.deleteNamespacedSecret(name, namespace, null, null, null, null, null, null).block().body()
     }
 }
