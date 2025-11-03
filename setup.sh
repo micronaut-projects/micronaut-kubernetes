647360b298ce954493c6c0eb89667fa6f11d7c30
Milenko Supic <milenko.supic@oracle.com>
2025-11-03 11:44:12 +0100

    Test scripts

--- setup.sh
+++ setup.sh
@@ -7,8 +7,13 @@ MODULE_NAME="${1%:nativeTest}"
 
 echo "Module Name: $MODULE_NAME"
 
-EXAMPLE_SERVICE_RUNTIME=${EXAMPLE_SERVICE_RUNTIME:="java"}
+kind version
+
+#EXAMPLE_SERVICE_RUNTIME=${EXAMPLE_SERVICE_RUNTIME:="java"}
+
+EXAMPLE_SERVICE_RUNTIME="java"
 
 if [ "$MODULE_NAME" == "examples:example-client" ]; then
-  ./setup-kubernetes.sh -c "test" -t "${EXAMPLE_SERVICE_RUNTIME}" -m "$MODULE_NAME,examples:example-service"
+  #./setup-kubernetes.sh -c "test" -t "${EXAMPLE_SERVICE_RUNTIME}" -m "$MODULE_NAME,examples:example-service"
+  #./setup-example-images.sh -t "${EXAMPLE_SERVICE_RUNTIME}" -m "$MODULE_NAME,examples:example-service"
 fi
