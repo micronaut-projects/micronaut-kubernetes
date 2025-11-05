cbd15da50b06d1727b235c2e582717ed7f75a43d
Milenko Supic <milenko.supic@oracle.com>
2025-11-05 10:03:08 +0100

    Test scripts

--- examples/example-client/src/test/groovy/micronaut/client/HelloControllerSpec.groovy
+++ examples/example-client/src/test/groovy/micronaut/client/HelloControllerSpec.groovy
@@ -54,7 +54,7 @@ class HelloControllerSpec extends KubernetesSpecification {
 
     void "test enemies"() {
         expect:
-        testClient.enemies().equals("noGoodRotten")
+        testClient.enemies().equals("noGoodRottenTEMP")
     }
 
     void "test config"() {
